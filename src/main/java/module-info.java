e3d96dbf357d5dfa1677e68b6f023aa7ba4a96fc
Aman Jain <amanj120@gmail.com>
2021-10-03 13:44:37 -0400

    pull

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.example.kishoreskomrades {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens com.example.kishoreskomrades to javafx.fxml;
     exports com.example.kishoreskomrades;
 }

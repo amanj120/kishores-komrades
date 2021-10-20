dd01aec5ef0a8868fb6cdf2316ca7cdcd4e5393f
Aman Jain <amanj120@gmail.com>
2021-10-20 10:20:03 -0400

    java11 for ubuntu

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,10 @@
 module com.example.kishoreskomrades {
+    requires javafx.base;
+    requires javafx.graphics;
     requires javafx.controls;
-    requires javafx.fxml;
-    requires junit;
+//    requires javafx.controls;
+//    requires javafx.fxml;
+//    requires junit;
 
     opens com.example.kishoreskomrades to javafx.fxml;
     exports com.example.kishoreskomrades;

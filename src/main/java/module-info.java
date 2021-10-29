fdbf37b8c3227849542cd2ec39814af1be557647
Jain, Aman <aman.jain@gatech.edu>
2021-10-29 15:02:38 -0400

    ok so it works now fr on windows

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,11 +2,10 @@ module com.example.kishoreskomrades {
     requires javafx.base;
     requires javafx.graphics;
     requires javafx.controls;
-//    requires javafx.controls;
-//    requires javafx.fxml;
-//    requires junit;
+    requires javafx.fxml;
+    requires junit;
 
-//    opens com.example.kishoreskomrades to javafx.fxml;
+    opens com.example.kishoreskomrades to javafx.fxml;
     exports com.example.kishoreskomrades;
     exports models;
 }

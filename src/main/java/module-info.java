b19a4a8f8d217031fcdc265fafcaaf9a41b75ad7
Jain, Aman <aman.jain@gatech.edu>
2021-10-31 16:19:03 -0400

    added stuff to gitignore and also modified game over string

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.kishoreskomrades {
+    requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires junit;
+
+    opens com.example.kishoreskomrades to javafx.fxml;
+    exports com.example.kishoreskomrades;
+    exports models;
+}
